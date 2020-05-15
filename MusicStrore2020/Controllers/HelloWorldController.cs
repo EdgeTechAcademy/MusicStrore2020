a61e31af6cef662672cbb9c74f8890f23188b4cf
Gary James <gjames@edgetechacademy.edu>
2020-05-14 22:33:37 -0500

    Add project files.

---
+++ MusicStrore2020/Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MusicStrore2020.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Me()
+        {
+            string[] ar = { "Julius", "Mark", "Gary", "Edge Tech Academy" };
+            ViewBag.List = ar;
+
+            Random rnd = new Random();
+            ViewBag.Age = (int)rnd.Next(100);
+            ViewBag.Name = "Gary James";
+            ViewBag.Date = DateTime.Now;
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "...Edge Tech has you...";
+        }
+    }
+}
---
+++ MusicStrore2020/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MusicStrore2020.Models;
+
+namespace MusicStrore2020.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MusicStrore2020.Models.Movie> Movies { get; set; }
+        public DbSet<MusicStrore2020.Models.Song> Songs { get; set; }
+    }
+}
